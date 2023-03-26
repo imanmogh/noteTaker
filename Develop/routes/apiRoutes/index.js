f1e0478edbb894d7b68f5074066922f2e2484f50
Iman Moghaddas <imanmoghaddas@yahoo.com>
2023-03-26 11:01:03 -0400

    cleaned up the code as well as added the code for the api routes and the thml routes

--- Develop/routes/apiRoutes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require(`express`).Router();
-const noteRoute = require(`./noteRoutes`);
-
-router.use(noteRoute);
-
-module.exports = router;
--- Develop/routes/apiRoutes/noteRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require(`express`).Router();
-const {notes} = require(`../../db/notes_db.json`);
-const {createNote, deleteNote} = require(`../../lib/noteFunction`);
-
-router.get(`./notes.html`, (req,res) => {
-    req.body.id = notes.length.toString();
-    let newNote = createNote(req.body, notes);
-    res.json(newNote);
-})
